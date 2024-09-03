683c43d0b86bf4987290bfa85c86284b26be0845
gyehwachae <gyehwa.chae@gmail.com>
2024-09-03 14:27:48 +0900

    first commit

---
+++ selenium/google.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/imghp?hl=ko&ogbl")
+elem = driver.find_element(By.NAME, "q")
+elem.send_keys("백구 강아지")
+elem.send_keys(Keys.RETURN)
+driver.find_elements(By.CSS_SELECTOR, ".uhHOwf.BYbUcd")[0].click()
+# assert "Python" in driver.title
+# elem = driver.find_element(By.NAME, "q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
